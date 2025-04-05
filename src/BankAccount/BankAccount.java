de57111f61511bf75ed1a0ca8ff37e1d1934b76f
Lawalolabode <lawalsulaimon003@gmail.com>
2025-04-05 20:10:16 +0100

    starting excution

--- src/BankAccount/BankAccount.java
+++ src/BankAccount/BankAccount.java
@@ -1,9 +1,45 @@
 package BankAccount;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class BankAccount {
     public static void main(String[] args) {
 
+        Scanner input = new Scanner(System.in);
+         System.out.println("Welcome to the FirstBank");
 
+         System.out.println("""
+    Welcome Select Any of the options below.
+    1. Create an Account.
+    2. For existing customer.
+    """);
+     System.out.print("Enter your option: ");
+        int option;
+     while(true) {
+         try {
+             option = input.nextInt();
+             while(option < 1 || option > 2) {
+                 System.out.println("Please enter a number between 1 and 2.");
+                 option = input.nextInt();
+             }
+
+         } catch (InputMismatchException e) {
+             System.out.println("Please enter an integer.");
+             input.nextLine();
+             continue;
+         }
+         break;
+
+     }
+
+     switch(option) {
+         case 1:
+
+             break;
+         case 2:
+             break;
+     }
 
 
     }
