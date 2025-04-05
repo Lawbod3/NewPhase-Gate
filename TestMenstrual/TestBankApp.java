ebcb582a63c0cd794e6506777d970017f7f15f58
Lawalolabode <lawalsulaimon003@gmail.com>
2025-04-05 13:31:46 +0100

    set deposit

--- TestMenstrual/TestBankApp.java
+++ TestMenstrual/TestBankApp.java
@@ -7,10 +7,41 @@ public class TestBankApp {
     BankAppMethod bankApp = new BankAppMethod();
 
     @Test
-    public void testGetBalance() {
-         bankApp.setPin("1234");
-        assertTrue(bankApp.validatePin("1234"));
+    public void testSetPin() {
+          bankApp.setPin("1234");
     }
+    @Test
+    public void testValidatePin() {
+        bankApp.setPin("1234");
+        String password = "1234";
+        assertTrue(bankApp.validatePin(password));
+    }
+    @Test
+    public void testAddAccount() {
+        bankApp.setAccountUser("Lawal","Olabode");
+    }
+    @Test
+    public void testGetAccountUser() {
+        bankApp.setAccountUser("Lawal","Olabode");
+        assertEquals("Lawal Olabode", bankApp.getAccountUser());
+    }
+    @Test
+    public void testGetBalance() {
+        double expected = 0.00;
+        assertEquals(expected, bankApp.getBalance(), 0.00);
+    }
+    @Test
+    public void testAddDeposit() {
+        bankApp.addDeposit(500.00);
+        bankApp.addDeposit(1000.00);
+        assertEquals(1500.00, bankApp.getBalance(), 0.00);
+
+
+    }
+
+
+
+
 
 }
 
--- src/BankAccount/BankAppMethod.java
+++ src/BankAccount/BankAppMethod.java
@@ -3,12 +3,36 @@ package BankAccount;
 import java.math.BigDecimal;
 
 public class BankAppMethod {
+    private double balance = 0.00;
     private String pin;
+    private String lastName;
+    private String firstName;
+
+    public void setPin(String number) {
+        this.pin = number;
+    }
+
+    public boolean validatePin(String password) {
+       if(password.equals(pin)) return true;
+      return false;
+    }
+
+    public void setAccountUser(String lawal, String olabode) {
+        this.lastName = lawal;
+        this.firstName = olabode;
+    }
 
 
+    public String getAccountUser() {
+        return this.lastName + " " + this.firstName ;
+    }
+
+    public double getBalance() {
+        return this.balance;
+    }
 
 
-    public void setPin(String password) {
-        this.pin = password;
+    public void addDeposit(double amount) {
+        this.balance += amount;
     }
 }
