e8ceb646a5c13d2ddb483b70b43102e6a749dcd8
Lawalolabode <lawalsulaimon003@gmail.com>
2025-04-07 00:05:37 +0100

    Still on the object

--- TestMenstrual/TestingMenstrual.java
+++ TestMenstrual/TestingMenstrual.java
@@ -1,2 +1,48 @@
+import MenstrualMethodFolder.NewMenstrualMethod;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
 public class TestingMenstrual {
+    @Test
+    public void testSetPeriodDays() {
+        NewMenstrualMethod period = new NewMenstrualMethod();
+        int days = 28;
+        period.setPeriodDays(28);
+    }
+    @Test
+    public void testGetNextPeriod() {
+        NewMenstrualMethod period = new NewMenstrualMethod();
+        int days = 28;
+        period.setPeriodDays(28);
+        assertEquals("05/05/2025",period.getNextPeriodDays());
+    }
+    @Test
+    public void testSetPeriodDaysLimit() {
+        NewMenstrualMethod period = new NewMenstrualMethod();
+        int days = 36;
+        assertThrows(IllegalArgumentException.class, () -> period.setPeriodDays(days));
+    }
+    @Test
+    public void testGetNextOvulation() {
+        NewMenstrualMethod period = new NewMenstrualMethod();
+        period.setPeriodDays(28);
+        assertEquals("19/05/2025", period.getNextOvulation());
+        period.setPeriodDays(30);
+        assertEquals("21/05/2025", period.getNextOvulation());
+        period.setPeriodDays(34);
+        assertEquals("25/05/2025", period.getNextOvulation());
+
+    }
+    @Test
+    public void testGetNextFertilityPeriod() {
+        NewMenstrualMethod period = new NewMenstrualMethod();
+        period.setPeriodDays(28);
+        assertEquals("14/05/2025 - 19/05/2025", period.getFertilityPeriod());
+        period.setPeriodDays(21);
+        assertEquals("03/05/2025 - 07/05/2025", period.getFertilityPeriod());
+        period.setPeriodDays(30);
+        assertEquals("16/05/2025 - 21/05/2025", period.getFertilityPeriod());
+
+    }
 }
---
+++ src/MenstrualMethodFolder/NewMenstrualMethod.java
@@ -0,0 +1,56 @@
+package MenstrualMethodFolder;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+public class NewMenstrualMethod {
+    private int periodDays;
+
+
+    public void setPeriodDays(int days) {
+        if (days < 21 || days > 35) throw new IllegalArgumentException("Days must be between 21 to 35");
+        this.periodDays = days;
+    }
+
+    public String getNextPeriodDays() {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        LocalDate today = LocalDate.now();
+        LocalDate nextPeriod = LocalDate.now().plusDays(periodDays);
+        return nextPeriod.format(formatter);
+    }
+
+    public String getNextOvulation() {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        LocalDate today = LocalDate.now();
+        LocalDate nextOvulation = LocalDate.now().plusDays(periodDays + 9);
+        if (periodDays > 24) {
+            LocalDate nextOvulation2 = LocalDate.now().plusDays(periodDays + 14);
+            return nextOvulation2.format(formatter);
+        }
+        else if (periodDays > 28) {
+            LocalDate nextOvulation3 = LocalDate.now().plusDays(periodDays + 16);
+            return nextOvulation3.format(formatter);
+        }
+        else if (periodDays > 33) {
+            LocalDate nextOvulation4 = LocalDate.now().plusDays(periodDays + 21);
+            return nextOvulation4.format(formatter);
+        }
+        return nextOvulation.format(formatter);
+
+    }
+
+    public String getFertilityPeriod() {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        LocalDate today = LocalDate.now();
+        LocalDate nextFertilityPeriod = LocalDate.now().plusDays(periodDays + 5);
+        if(periodDays > 24) {
+            LocalDate nextFertilityPeriod2 = LocalDate.now().plusDays(periodDays + 9);
+            return String.valueOf(nextFertilityPeriod2.format(formatter)) + " - " + String.valueOf(getNextOvulation());
+        }
+        else if (periodDays > 28) {
+            LocalDate nextFertilityPeriod3 = LocalDate.now().plusDays(periodDays + 11);
+            return String.valueOf(nextFertilityPeriod3.format(formatter)) + " - " + String.valueOf(getNextOvulation());
+        }
+        return String.valueOf(nextFertilityPeriod.format(formatter)) + " - " + String.valueOf(getNextOvulation());
+    }
+}
