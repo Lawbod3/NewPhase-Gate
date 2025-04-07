be8bb33a50ebb0ce5c9e826c735f9e9c856d6ce4
Lawalolabode <lawalsulaimon003@gmail.com>
2025-04-07 22:58:43 +0100

    menstrual almost done

--- src/MenstrualAndOthers/NewMenstrual.java
+++ src/MenstrualAndOthers/NewMenstrual.java
@@ -64,11 +64,12 @@ public class NewMenstrual {
         }
 
         System.out.print("""
-     Kindly Select any of the options to let me know the reason for using the App?
+     Whats your reason for using the App?
      1. Track Menstrual Cycle.
      2. Fertility And Family Planing.
      """);
         String menuChoice;
+        int choice;
         while (true) {
             try{
                 System.out.print("Enter your choice: ");
@@ -76,7 +77,7 @@ public class NewMenstrual {
                 if(menuChoice.isEmpty()){
                     continue;
                 }
-                int choice = Integer.parseInt(menuChoice);
+                choice = Integer.parseInt(menuChoice);
                 while (choice < 1 || choice > 2) {
                     System.out.print("Please enter NUMBER from 1 to 2: ");
                     keyboard.nextLine();
@@ -84,14 +85,34 @@ public class NewMenstrual {
                 }
                 keyboard.nextLine();
 
-
             } catch (InputMismatchException e) {
-
                 System.out.print("Please enter a valid option!");
                 keyboard.nextLine();
                 continue;
             }
-
+            switch(choice) {
+                case 1:
+                    System.out.println("You selected Track Menstrual Cycle.");
+                    System.out.printf("""
+                       Your next Menstrual period is likely to be on the %s%n
+                       Your next Menstrual flow will be within this days %s%n
+                       Your next Ovulation is likely to be on the %s%n
+                       Your Pre Ovulation period will be within this days %s%n
+                       Your Ovulation is likely to be on this day %s%n
+                       Your Post Ovulation period will be within this days %s
+                       Your End of next menstrual Cycle is likely to be on the %s%n
+                       """,period.getNextPeriodDays(), period.getNextFlowDays(), period.getNextOvulation(), period.getNextPreOvulation(),  period.getNextPostOvulation(), period.getEndOfNextCycle());
+                    break;
+                case 2:
+                    System.out.println("You selected Fertility And Family Planing..");
+                    System.out.printf("""
+                       Low Probability of pregnancy is likely to be within this period %s%n
+                       High Probability of pregnancy is likely to be within this period %s%n
+                       Ovulation day have the highest probability of Pregnancy and is likely to be on this day %s%n
+                       """, period.getFertilityPeriod(), period.getNextOvulation());
+                    break;
+            }
+          break;
         }
 
         }
