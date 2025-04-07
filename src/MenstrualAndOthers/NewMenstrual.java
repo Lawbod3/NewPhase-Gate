3c7f8d443fa21cf27258ceb89ab5d1ac292c779e
Lawalolabode <lawalsulaimon003@gmail.com>
2025-04-07 21:29:45 +0100

    menstrual

--- src/MenstrualAndOthers/NewMenstrual.java
+++ src/MenstrualAndOthers/NewMenstrual.java
@@ -6,6 +6,7 @@ import MenstrualMethodFolder.NewMenstrualMethod;
 import java.time.DateTimeException;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class NewMenstrual {
@@ -18,7 +19,7 @@ public class NewMenstrual {
         System.out.println("Welcome to Women Menstrual Cycle");
         System.out.println("We will be asking some personal information");
 
-        System.out.print("When was the first day of your last period?(dd/MM/yyyy): ");
+        System.out.print("\nWhen was the first day of your last period?(dd/MM/yyyy): ");
         while(true) {
             try {
                 String firstDay = keyboard.nextLine();
@@ -31,13 +32,77 @@ public class NewMenstrual {
             }
             break;
         }
+        System.out.print("How many days does your Menstrual Cycle last: ");
+        while(true) {
+            String days = keyboard.nextLine();
+           while(!nullValidate(days)){
+               System.out.print("Invalid input, please try again make sure it two digit numbers: ");
+               days = keyboard.nextLine();
+           }
+           int day = Integer.parseInt(days);
+           if(day < 21 || day > 35){
+               System.out.print("menstrual cycle last within this period 21 - 31days, please go see a doctor");
+               break;
+           }
+           period.setPeriodDays(day);
+            break;
+        }
+        while(true) {
+            System.out.print("How many days does your flow days last: ");
+            String flowDays = keyboard.nextLine();
+            while(!flowValidate(flowDays)){
+                System.out.print("Invalid input, please try again: ");
+                flowDays = keyboard.nextLine();
+            }
+            int flowday = Integer.parseInt(flowDays);
+            if(flowday < 1 || flowday > 7){
+                System.out.print("bleeding more than 7days or not bleeding at all, please go see a doctor");
+                break;
+            }
+            period.setFlowDays(flowday);
+            break;
+        }
+
+        System.out.print("""
+     Kindly Select any of the options to let me know the reason for using the App?
+     1. Track Menstrual Cycle.
+     2. Fertility And Family Planing.
+     """);
+        String menuChoice;
+        while (true) {
+            try{
+                System.out.print("Enter your choice: ");
+                menuChoice = keyboard.nextLine();
+                if(menuChoice.isEmpty()){
+                    continue;
+                }
+                int choice = Integer.parseInt(menuChoice);
+                while (choice < 1 || choice > 2) {
+                    System.out.print("Please enter NUMBER from 1 to 2: ");
+                    keyboard.nextLine();
+                    choice = keyboard.nextInt();
+                }
+                keyboard.nextLine();
 
 
+            } catch (InputMismatchException e) {
 
+                System.out.print("Please enter a valid option!");
+                keyboard.nextLine();
+                continue;
+            }
 
+        }
 
+        }
+    public static boolean nullValidate(String option) {
+        if(option.length()  == 2 && option.matches("^[0-9]+$") && (!option.isEmpty())) return true;
+        return false;
+    }
 
-
+    public static boolean flowValidate(String option) {
+        if(1 == option.length() && option.matches("^[0-9]+$") && (!option.isEmpty())) return true;
+        return false;
     }
 
 }
